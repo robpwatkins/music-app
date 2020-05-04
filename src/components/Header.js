728a06c63c59456a53584bd54fb0f066cab19deb
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-05-03 21:25:01 -0500

    testing that change

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { AppBar, Toolbar } from '@material-ui/core';
 function Header () {
   return (
     <AppBar position="static">
-      <Toolbar>Music App</Toolbar>
+      <Toolbar>Music App, B.</Toolbar>
     </AppBar>
   )
 }
