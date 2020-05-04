3991c1e5a72e24be75623236535acae65637a10c
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2020-05-03 21:22:29 -0500

    Updates

---
+++ precache-manifest.25c87e656f5881f0e67f7abdab570fc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f54cba6d1dd94dd4af176ef9b2fbef",
+    "url": "/music-app/index.html"
+  },
+  {
+    "revision": "f22cce5437825a7e4d21",
+    "url": "/music-app/static/css/main.0924a6a3.chunk.css"
+  },
+  {
+    "revision": "4aef181595c53382bb27",
+    "url": "/music-app/static/js/2.1f67288b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/music-app/static/js/2.1f67288b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22cce5437825a7e4d21",
+    "url": "/music-app/static/js/main.81d3e220.chunk.js"
+  },
+  {
+    "revision": "5951a6ddcf253209c019",
+    "url": "/music-app/static/js/runtime-main.65d8b084.js"
+  }
+]);
